e832ae71ce9de853e392f77ebaf490cb6fb20e42
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-01 23:47:10 +0600

    Complete: Handle model and urls in Django ✅ 👍

--- Start a Django project and file structure/chai-django/chaiaurDjango/chai/models.py
+++ Start a Django project and file structure/chai-django/chaiaurDjango/chai/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPES_CHOICES = [
+        ('ML', "MASALA"),
+        ('GR', 'GINGER'),
+        ('KL', 'KIWI'),
+        ('PL', 'PLAIN'),
+        ('EL', 'ELACHI'),
+    ]
+    name = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to  = 'chais/')
+    date_added = models.DateTimeField(default = timezone.now)
+    type = models.CharField(max_length = 2, choices = CHAI_TYPES_CHOICES)
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return self.name
