ef01b6c7e4acf0990bab0388d1abe30df739f0f0
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-04 00:04:32 +0600

    root home tamplates added with config.

--- self_practices/assaignment_1/search_tweets/search_tweets/settings.py
+++ self_practices/assaignment_1/search_tweets/search_tweets/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'search_tweets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- self_practices/assaignment_1/search_tweets/search_tweets/views.py
+++ self_practices/assaignment_1/search_tweets/search_tweets/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
