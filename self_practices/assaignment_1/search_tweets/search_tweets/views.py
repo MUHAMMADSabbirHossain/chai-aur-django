ce191a14d611bbb4c2600665e185dda5b0a90a34
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-04 00:46:10 +0600

    jinja tamplates and tailwind cdn added.

--- self_practices/assaignment_1/search_tweets/search_tweets/views.py
+++ self_practices/assaignment_1/search_tweets/search_tweets/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 def home(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'index.html')
+
+    context = {
+        "current_year": datetime.date.today().year,
+    }
+      
+    return render(request, 'index.html', context)
