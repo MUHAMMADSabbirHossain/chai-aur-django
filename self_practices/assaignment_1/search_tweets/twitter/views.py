d0998bbae9521b03c603333034d49085ced9f8a6
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-05 14:45:15 +0600

    Single tweet item showing in the app.

--- self_practices/assaignment_1/search_tweets/twitter/views.py
+++ self_practices/assaignment_1/search_tweets/twitter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tweet 
 
 # Create your views here.
 def tweet_list(request):
-    return render(request, 'tweet_list.html')
+    tweets = Tweet.objects.all().order_by('-created_at')
+
+    return render(request, 'tweet_list.html', {'tweets': tweets})
