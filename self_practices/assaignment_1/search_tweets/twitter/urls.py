bf760541b2cefc00ddcfc938dc5d1b3ee9cdadc3
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-05 15:17:54 +0600

    Single tweet detail functionality added.

--- self_practices/assaignment_1/search_tweets/twitter/urls.py
+++ self_practices/assaignment_1/search_tweets/twitter/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tweet_list, name='tweet_list'),
+    path('<int:tweet_id>/', views.tweet_detail, name='tweet_detail'),
 ]
--- self_practices/assaignment_1/search_tweets/twitter/views.py
+++ self_practices/assaignment_1/search_tweets/twitter/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Tweet 
 
 # Create your views here.
 def tweet_list(request):
-    tweets = Tweet.objects.all().order_by('-created_at')
+    tweets = Tweet.objects.all().order_by('-created_at',)
 
     return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_detail(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id)
+    
+    return render(request, 'tweet_detail.html', {'tweet': tweet})
